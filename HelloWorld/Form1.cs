341f46cc9edc6170a310fbfcf09b60e77cfcb072
Sidney Faria <sidneyfaria@hotmail.com>
2018-05-20 11:41:43 -0400

    teste commit 02

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello World. HI!");
         }
     }
 }
